bf172393a915e3469693b7393aba13137637be3b
Iyyamperumal Arumugam <ip.iyyam@gmail.com>
2025-09-19 03:07:00 +0530

    feat: revert _document.tsx back to default

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* ✅ Google Fonts */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&family=Merriweather:wght@400;700&family=Playfair+Display:wght@400;700&family=Fira+Code:wght@400;500;700&family=Source+Code+Pro:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body className="font-serif bg-white text-dark">
+      <Head />
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
